69087fc7feead848b5397d7c0608a41d214f50e1
TheMagicalBlob <alteringorbisgames@gmail.com>
2025-06-09 21:36:23 -0700

    1.1.8: updated assembly name.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("lookupSID")]
+[assembly: AssemblyTitle("SIDHelper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("lookupSID")]
+[assembly: AssemblyProduct("SIDHelper")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
